4e271312f6d7bb60b532d4ddff33237b86cac82f
Alexandre Hertzog <alex.hertzog@outlook.com>
2022-08-21 12:51:41 +0100

    Adds multiple cubes

--- examples/hello-world/src/hello_world.cpp
+++ examples/hello-world/src/hello_world.cpp
@@ -97,10 +97,17 @@ int main() {
         square.get_program()->set_uniform("texture1", 0);
         square.get_program()->set_uniform("texture2", 1);
 
-        square.get_program()->set_use_callback([](opengl_wrapper::program &p) {
+        std::vector<glm::vec3> cube_positions = {glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+                                                 glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+                                                 glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+                                                 glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+                                                 glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
+
+        auto cube_index = 0;
+        square.get_program()->set_use_callback([&](opengl_wrapper::program &p) {
             auto model = glm::mat4(1.0F);
-            model = glm::rotate(model, static_cast<float>(glfwGetTime()) * glm::radians(50.0F),
-                                glm::vec3(0.5F, 1.0F, 0.0F));
+            model = glm::translate(model, cube_positions.at(cube_index));
+            model = glm::rotate(model, glm::radians(20.0F * cube_index), glm::vec3(1.0F, 0.3F, 0.5F));
 
             auto view = glm::mat4(1.0F);
             view = glm::translate(view, glm::vec3(0.0F, 0.0F, -3.0F));
@@ -110,11 +117,17 @@ int main() {
             p.set_uniform("model", glm::value_ptr(model));
             p.set_uniform("view", glm::value_ptr(view));
             p.set_uniform("projection", glm::value_ptr(projection));
+
+            if (cube_index++ >= cube_positions.size() - 1) {
+                cube_index = 0;
+            }
         });
 
         square.set_textures({std::move(container_texture), std::move(awesomeface_texture)});
 
-        window.get_renderer().add_shape(std::move(square));
+        for (auto i = 0; i < cube_positions.size(); i++) {
+            window.get_renderer().add_shape(square);
+        }
 
         window.render_loop();
         return 0;
